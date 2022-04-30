89e18f3aeffe69c67c7a787a4330915674faa778
ximki-vinki <chem-ixtrems@mail.ru>
2022-04-30 12:48:19 +0300

    Add project files.

---
+++ Sudoku Solution Validator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Sudoku_Solution_Validator
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[][] board = new int[][]
+        {
+          new int[] {8, 2, 6, 3, 4, 7, 5, 9, 1},
+          new int[] {7, 3, 5, 8, 1, 9, 6, 4, 2},
+          new int[] {1, 9, 4, 2, 6, 5, 8, 7, 3},
+          new int[] {3, 1, 7, 5, 8, 4, 2, 6, 9},
+          new int[] {6, 5, 9, 1, 7, 2, 4, 3, 8},
+          new int[] {4, 8, 2, 9, 3, 6, 7, 1, 5},
+          new int[] {9, 4, 8, 7, 5, 1, 3, 2, 6},
+          new int[] {5, 6, 1, 4, 2, 3, 9, 8, 7},
+          new int[] {2, 7, 3, 6, 9, 8, 1, 5, 4},
+        };
+            
+
+            Console.WriteLine(Sudoku.ValidateSolution(board));
+
+        }
+
+    }
+
+}
---
+++ Sudoku Solution Validator/Sudoku.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Sudoku_Solution_Validator
+{
+    internal class Sudoku
+    {
+        public static bool ValidateSolution(int[][] board)
+        {
+            int[] arrayToCheck = new int[9];
+            for (int i = 0; i < 9; ++i)
+            {
+                for (int j = 0; j < 9; ++j)
+                {
+                    arrayToCheck[j] = board[i][j];
+                }
+                if (!CheckNumber(arrayToCheck)) return false;
+            }
+            for (int i = 0; i < 9; ++i)
+            {
+                for (int j = 0; j < 9; ++j)
+                {
+                    arrayToCheck[j] = board[j][i];
+                }
+                if (!CheckNumber(arrayToCheck)) return false;
+            }
+            for (int i = 0; i < 9; ++i)
+            {
+                for (int j = 0; j < 9; ++j)
+                {
+                    arrayToCheck[j] = board[j / 3 + i % 3 * 3][j % 3 + i / 3 * 3];
+                }
+                if (!CheckNumber(arrayToCheck)) return false;
+            }
+            return true;
+        }
+        private static bool CheckNumber(int[] checkArray)
+        {
+            int[] allNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            return allNumbers.All(checkArray.Contains);
+        }
+    }
+}
